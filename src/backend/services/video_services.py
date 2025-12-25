f7821f49fb1e752fe158bde4b29881803cb8efcd
NamQuanProject <namquanproject@gmail.com>
2025-12-25 13:00:31 +0700

    Add user video backend

---
+++ src/backend/services/video_services.py
@@ -0,0 +1,78 @@
+from ..database import supabase
+from ..utils import videos
+import os
+from ..services.ai_service import classify_video, summarize_video, get_location
+import json
+import re
+import time
+
+def add_user_video(url: str, user_id: str):
+    """
+    Downloads a video from the given URL and saves it to the storage.
+    If the video already exists in the database, it does not download it again.
+    Returns the video data if successful, or an error message if not.
+    """
+
+    user_existed = supabase.table("User").select("*").eq("id", user_id).execute()
+    if not user_existed.data:
+        return None, "User does not exist."
+
+    video_existed = supabase.table("Video").select("*").eq("video_url", url).execute()
+    if not video_existed.data:
+        save_dir = os.path.join('storage', 'videos', 'downloaded')
+        
+        # Download video and upload to the bucket
+        public_url, vid_desc, error = videos.download_video_sstik(url=url, save_dir=save_dir)
+
+        # public_url, vid_desc, error = videos.download_video_tiktok(url=url, save_dir=save_dir)
+        if error:
+            print(f"Error downloading video: {error}")
+            return None, error
+
+        print("public_url:", public_url)
+
+        # Process video
+        vid_type = classify_video(public_url)
+        time.sleep(2)
+        video_info = summarize_video(type=vid_type, video_url=public_url)
+
+        # Insert video to database
+        result = supabase.table("Video").insert({
+            # 'user_id': user_id,
+            'type': vid_type,
+            'description': vid_desc,
+            'video_url': url,
+            'bucket_url': public_url,
+            'raw_info': video_info
+        }).execute()
+
+        video_id = result.data[0]['id'] if result.data else None
+
+
+
+    else:
+        print("[add_user_video] Video has already existed")
+        video_id = video_existed.data[0]['id']
+        vid_type = video_existed.data[0]['type']
+        video_info = video_existed.data[0]['raw_info']
+
+    user_video_existed = supabase.table("UserVideo").select("*").eq("user_id", user_id).eq("video_id", video_id).execute()
+
+    if not user_video_existed:
+        result = supabase.table("UserVideo").insert({
+            'user_id': user_id,
+            'video_id': video_id
+        }).execute()
+
+    else:
+        result = user_video_existed
+
+    if vid_type == 0: # Food Review
+        add_foodstores(user_id, video_info)
+    
+    elif vid_type == 1: # Cooking Guide
+        add_recipes(user_id, video_info)
+        
+    return result.data, None
+
+
