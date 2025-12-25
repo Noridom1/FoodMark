c0124dcecd642945ff0d364a3825afa7dd9b91bd
NamQuanProject <namquanproject@gmail.com>
2025-12-25 13:11:55 +0700

    Adding the main for the getting route services for ai and video recommend

--- src/backend/app/main.py
+++ src/backend/app/main.py
@@ -1,43 +1,52 @@
-import os
-import torch
-from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from loguru import logger
+from .database import supabase
+from .services import ai_service
+from fastapi import FastAPI, Form
+from fastapi import Query
+from typing import List
+from fastapi import Body
 
-# from backend.routers import (
-#     auth, user
-# )
+
+app = FastAPI()
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
+
+
+
+@app.get("/")
+def root():
+    return {"message": "Backend is running!"}
+
+
+@app.get("/current_user")
+def test_supabase():
+    # Example: fetch from a table called 'users'
+    response = supabase.auth.get_user()
+    return response
+
+@app.get("/bucket/{name}")
+def get_a_bucket(name: str):
+    buckets = supabase.storage.list_buckets()
     
-    yield
-    logger.info("Shutting down app")
+    print(buckets)
 
-
-
-def create_app():
-    app = FastAPI(title="FoodMark App", version="1.0.0", lifespan=lifespan)
-
-    allowed = os.getenv("ALLOWED_ORIGINS", "http://localhost:5173").split(",")
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=[o.strip() for o in allowed],
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
+@app.get("/summarize")
+def summarize_video():
+    data = ai_service.summarize_video(
+        type=1,
+        video_url="https://fgkmsasdgcykscfcsynx.supabase.co/storage/v1/object/public/videobucket/BaChiXotTac.mp4"
     )
+
+    print(data)
     
-    # app.include_router(auth.router)
-    # app.include_router(user.router)
-    
-    return app
+    return {
+        "status": "success",
+        "data": data
+    }
 
 
-app = create_app()
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+@app.get('/classifier')
+def classify_video():
+    ai_service.classify_video(
+        video_url="https://plsnwavugnuoppecuogh.supabase.co/storage/v1/object/public/videos/ComLuoi.mp4"
+    )
